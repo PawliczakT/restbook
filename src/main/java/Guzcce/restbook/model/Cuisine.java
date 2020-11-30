f30492b733be47a1b0d92baf92393c1b8519df07
tgruza <tgruza@wp.pl>
2020-11-30 21:44:27 +0100

    model changes

--- src/main/java/Guzcce/restbook/model/Cuisine.java
+++ src/main/java/Guzcce/restbook/model/Cuisine.java
@@ -1,4 +1,38 @@
 package Guzcce.restbook.model;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table
 public class Cuisine {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+
+    public Cuisine() {
+    }
+
+    public Cuisine(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/Guzcce/restbook/model/Review.java
+++ src/main/java/Guzcce/restbook/model/Review.java
@@ -1,4 +1,47 @@
 package Guzcce.restbook.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table
 public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String description;
+    private float rate;
+
+    public Review() {
+    }
+
+    public Review(Long id, String description, float rate) {
+        this.id = id;
+        this.description = description;
+        this.rate = rate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getRate() {
+        return rate;
+    }
+
+    public void setRate(float rate) {
+        this.rate = rate;
+    }
 }
